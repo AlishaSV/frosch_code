53496ac14d8d47cc93c723535ac8e8af1c49c95a
alina <alina.svetlaya@gmail.com>
2023-01-27 16:07:55 +0100

    created new components and routes

--- src/App.js
+++ src/App.js
@@ -1,19 +1,27 @@
 import React from 'react'
 import './App.css'
 import Dialogs from './components/Dialogs/Dialogs'
-import { Route, Routes } from 'react-router-dom'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Root from './pages/Root'
 import Profile from './components/Profile/Profile'
+import News from './components/News/News'
+import Music from './components/Music/Music'
+import Settings from './components/Settings/Settings'
 
 const App = () => {
 
   return (
-    <Routes>
-      <Route path="/" element={<Root/>}>
-        <Route path="dialogs" element={<Dialogs/>}/>
-        <Route path="profile" element={<Profile/>}/>
-      </Route>
-    </Routes>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Root/>}>
+          <Route path="dialogs" element={<Dialogs/>}/>
+          <Route path="profile" element={<Profile/>}/>
+          <Route path="news" element={<News/>}/>
+          <Route path="music" element={<Music/>}/>
+          <Route path="settings" element={<Settings/>}/>
+        </Route>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
