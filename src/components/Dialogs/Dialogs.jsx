d63f4390562b7ee53479b94f07b82884ce111441
alina <alina.svetlaya@gmail.com>
2023-01-30 13:15:42 +0100

    refactoring dialogs component

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,9 +1,25 @@
 import React from 'react'
+import s from './Dialogs.module.css'
 
 const Dialogs = (props) => {
   return (
-    <div>
-      Dialogs
+    <div className={s.dialogs}>
+      <div className={s.dialogsItem}>
+        <div className={s.dialog + ' ' + s.active}>
+          Alina
+        </div>
+        <div className={s.dialog}>
+          Alex
+        </div>
+        <div className={s.dialog}>
+          Basil
+        </div>
+      </div>
+      <div className={s.messages}>
+        <div className={s.message}>Hi</div>
+        <div className={s.message}>You are frog</div>
+        <div className={s.message}>Water me please</div>
+      </div>
     </div>
   )
 }
