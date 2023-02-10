53ac227028f551e65f9ed51a67e48e9e7a23b321
alina <alina.svetlaya@gmail.com>
2023-02-10 11:56:12 +0100

    added new component

---
+++ src/components/Sidebar/Sidebar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import s from './Sidebar.module.css'
+
+const Sidebar = () => {
+  return (
+    <div className={s.appWrapperContent}>
+      Friends
+    </div>
+  )
+}
+
+export default Sidebar
