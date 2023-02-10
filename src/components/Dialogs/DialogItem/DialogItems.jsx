2f1256ba062207182dd3685cd52f37c35448995e
alina <alina.svetlaya@gmail.com>
2023-02-10 11:57:49 +0100

    create and add new classes

--- src/components/Dialogs/DialogItem/DialogItems.jsx
+++ src/components/Dialogs/DialogItem/DialogItems.jsx
@@ -2,9 +2,13 @@ import React from 'react'
 import NavLink from '../../NavLink'
 import s from './DialogItem.module.css'
 
-const DialogItem = ({ id, title }) => {
+const DialogItem = ({ id, title, avatar }) => {
+
   return (
-    <NavLink path={`/dialogs/${id}`} title={title} className={s.item} activeStyle={s.activeItem}/>
+    <div className={s.itemWrapper}>
+      <img alt={id} className={s.avatar} src={avatar}/>
+      <NavLink path={'/dialogs/' + id} title={title} className={s.link} activeStyle={s.activeLink}/>
+    </div>
   )
 }
 
--- src/components/Dialogs/Message/Message.jsx
+++ src/components/Dialogs/Message/Message.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
 import s from './Message.module.css'
 
-const Message = ({ message }) => {
+const Message = ({ message, avatar, id }) => {
   return (
-    <div className={s.message}>{message}</div>
+    <div className={s.dialogsWrapper}>
+      <img className={s.avatar} alt={id} src={avatar}/>
+      <div className={s.message}>{message}</div>
+    </div>
   )
 }
 
