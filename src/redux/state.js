c791dd7335e5c60087ebd74351412e50990ad827
alina <alina.svetlaya@gmail.com>
2023-02-10 11:53:20 +0100

    added object element to state.js

--- src/redux/state.js
+++ src/redux/state.js
@@ -7,14 +7,38 @@ let state = {
   },
   dialogsPage: {
     dialogsData: [
-      { id: 1, title: 'Alina' },
-      { id: 2, title: 'Alex' },
-      { id: 3, title: 'Basil' },
+      {
+        id: 1,
+        title: 'Alina',
+        avatar: 'https://images.unsplash.com/photo-1513245543132-31f507417b26?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2835&q=80'
+      },
+      {
+        id: 2,
+        title: 'Alex',
+        avatar: 'https://images.unsplash.com/photo-1513245543132-31f507417b26?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2835&q=80'
+      },
+      {
+        id: 3,
+        title: 'Basil',
+        avatar: 'https://images.unsplash.com/photo-1513245543132-31f507417b26?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2835&q=80'
+      },
     ],
     messagesData: [
-      { id: 1, message: 'Hi' },
-      { id: 2, message: 'You are frog' },
-      { id: 3, message: 'Water me please' },
+      {
+        id: 1,
+        message: 'Hi',
+        avatar: 'https://images.unsplash.com/photo-1513245543132-31f507417b26?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2835&q=80'
+      },
+      {
+        id: 2,
+        message: 'You are frog',
+        avatar: 'https://images.unsplash.com/photo-1513245543132-31f507417b26?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2835&q=80'
+      },
+      {
+        id: 3,
+        message: 'Water me please',
+        avatar: 'https://images.unsplash.com/photo-1513245543132-31f507417b26?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2835&q=80'
+      },
     ]
   }
 }
