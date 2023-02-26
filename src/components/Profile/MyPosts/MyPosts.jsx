48d11f84f1d9a1ac352a3af422aa99b7b3e1b030
alina <alina.svetlaya@gmail.com>
2023-02-26 17:51:44 +0100

    refactoring with bind

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -3,7 +3,7 @@ import s from './MyPosts.module.css'
 import Post from './Post/Post'
 
 const MyPosts = ({ postsData }) => {
-  
+
   return (
     <div className={s.posts}>
       <h4>My posts</h4>
@@ -23,9 +23,8 @@ const MyPosts = ({ postsData }) => {
       </div>
       <div className={s.posts}>
         <div>
-          {postsData.posts.map((post, index) => <Post key={index} {...post} deletePost={(id) => {
-            postsData.deletePostById(id)
-          }}/>)}
+          {postsData.posts.map((post, index) => <Post key={index} {...post}
+                                                      deletePost={postsData.deletePostById.bind(postsData)}/>)}
         </div>
       </div>
     </div>
