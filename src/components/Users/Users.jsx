0ff903db9856d345916b4b80a694d47c24f072aa
alina <alina.svetlaya@gmail.com>
2023-03-19 23:12:02 +0100

    added and implemented axios library

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,27 +1,36 @@
 import React from 'react'
 import styles from './Users.module.css'
+import axios from 'axios'
+import userPhoto from '../../images-store/images/icons8-cat-profile-100.png'
 
-let Users = ({ users, follow, unfollow }) => {
+let Users = ({ users, follow, unfollow, setUsers }) => {
+  let getUsers = () => {
+    axios.get('https://social-network.samuraijs.com/api/1.0/users').then(response => {
+      setUsers(response.data.items)
+    })
+  }
+
   return (
     <div>
+      <button onClick={getUsers}>Get users</button>
       {users.map((u) =>
-        (<div key={u.id}>
+        (<div key={u?.id}>
             <span>
               <div>
-                <img src={u.photoUrl} className={styles.userPhoto}/>
+                <img src={u?.photos?.small ?? userPhoto} className={styles.userPhoto}/>
               </div>
               <div>
-                {u.followed ? <button onClick={() => {unfollow(u.id)}}>Unfollow</button>
-                  : <button onClick={() => {follow(u.id)}}>Follow</button>}
+                {u?.followed ? <button onClick={() => {unfollow(u?.id)}}>Unfollow</button>
+                  : <button onClick={() => {follow(u?.id)}}>Follow</button>}
               </div>
             </span>
             <span>
               <span>
-                <div>{u.fullName}</div>
+                <div>{u?.name}</div>
               </span>
               <span>
-                <div>{u.location.city}</div>
-                <div>{u.location.country}</div>
+                <div>{u?.location?.city}</div>
+                <div>{u?.location?.country}</div>
               </span>
             </span>
           </div>
