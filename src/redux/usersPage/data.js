f2170295634aa5dc48170e5129fbe8c8197ba9d0
alina <alina.svetlaya@gmail.com>
2023-03-19 23:09:45 +0100

    erased users data

--- src/redux/usersPage/data.js
+++ src/redux/usersPage/data.js
@@ -1,25 +1,3 @@
 export const usersData = {
-  users: [
-    {
-      id: 1,
-      photoUrl: 'https://img.icons8.com/neon/512/person-male.png',
-      followed: false,
-      fullName: 'xxxA',
-      location: { city: 'Hamburg', country: 'Germany' }
-    },
-    {
-      id: 2,
-      photoUrl: 'https://img.icons8.com/neon/512/person-male.png',
-      followed: true,
-      fullName: 'xxxB',
-      location: { city: 'Zurich', country: 'Switzerland' }
-    },
-    {
-      id: 3,
-      photoUrl: 'https://img.icons8.com/neon/512/person-male.png',
-      followed: false,
-      fullName: 'xxxC',
-      location: { city: 'Rome', country: 'Italy' }
-    }
-  ]
+  users: []
 }
