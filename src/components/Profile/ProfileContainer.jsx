733dadd4796c75c9a95e3ff7c7cc3305f758a442
alina <alina.svetlaya@gmail.com>
2023-04-12 22:39:09 +0200

    refactoring

--- src/components/Profile/ProfileContainer.jsx
+++ src/components/Profile/ProfileContainer.jsx
@@ -3,7 +3,6 @@ import Profile from './Profile'
 import { connect } from 'react-redux'
 import { getUserProfileTC } from '../../redux/profile-reducer'
 import { withRouter } from '../utils'
-import { withAuthRedirect } from '../../hoc/withAuthRedirect'
 import { compose } from 'redux'
 
 class ProfileContainer extends React.Component {
@@ -32,6 +31,5 @@ let mapStateToProps = (state) => {
 
 export default compose(
   connect(mapStateToProps, { getUserProfileTC }),
-  withRouter,
-  withAuthRedirect
+  withRouter
 )(ProfileContainer)
