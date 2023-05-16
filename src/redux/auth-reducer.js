fc9ee507a14ae0869ea7d74d8c0b9c4440dc619e
alina <alina.svetlaya@gmail.com>
2023-05-16 13:16:42 +0200

    added is not logged in AC to auth reducer

--- src/redux/auth-reducer.js
+++ src/redux/auth-reducer.js
@@ -4,12 +4,16 @@ import { authMe, loginToApp } from '../api/api'
 let SET_USER_DATA = 'SET_USER_DATA'
 let LOGIN_TO_APP = 'LOGIN_TO_APP'
 let IS_LOADING = 'IS_LOADING'
+let IS_NOT_LOGIN_AFTER_LOADING = 'IS_NOT_LOGIN_AFTER_LOADING'
 
 const authReducer = (authData = initAuthData, action) => {
   const newAuthData = { ...authData }
   switch (action.type) {
     case IS_LOADING:
       newAuthData.authData = { ...newAuthData, isLoading: true }
+      break
+    case IS_NOT_LOGIN_AFTER_LOADING:
+      newAuthData.authData = { ...action.data, isAuth: false, isLoading: false }
       break
     case SET_USER_DATA:
       newAuthData.authData = { ...action.data, isAuth: true, isLoading: false }
@@ -24,7 +28,8 @@ const authReducer = (authData = initAuthData, action) => {
 }
 
 export const setAuthUserData = (userId, email, login) => ({ type: SET_USER_DATA, data: { userId, email, login } })
-export const setAuthUserInLoading = () => ({ type: IS_LOADING })
+export const setAuthUserInLoadingActionCreator = () => ({ type: IS_LOADING })
+export const setAuthUserIsNotLoginAfterLoadingActionCreator = () => ({ type: IS_NOT_LOGIN_AFTER_LOADING })
 export const setLoginToApp = (email, password, rememberMe) => ({
   type: LOGIN_TO_APP,
   data: { email, password, rememberMe }
@@ -32,12 +37,14 @@ export const setLoginToApp = (email, password, rememberMe) => ({
 
 export const getAuthUserData = () => {
   return dispatch => {
-    dispatch(setAuthUserInLoading)
+    dispatch(setAuthUserInLoadingActionCreator())
     authMe()
     .then(response => {
       if (response.data.resultCode === 0) {
         let { id, email, login } = response.data.data
         dispatch(setAuthUserData(id, email, login))
+      } else {
+        dispatch(setAuthUserIsNotLoginAfterLoadingActionCreator())
       }
     })
   }
